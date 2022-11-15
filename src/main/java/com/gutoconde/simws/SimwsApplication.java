4fa9bcd9298a5df7fcad489fd971144660273661
gutoconde <gutoconde@gmail.com>
2022-11-15 15:18:03 -0300

    Ajustes para diminuir o tempo de cold start

--- src/main/java/com/gutoconde/simws/SimwsApplication.java
+++ src/main/java/com/gutoconde/simws/SimwsApplication.java
@@ -2,10 +2,15 @@ package com.gutoconde.simws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.gutoconde.simws.api.controller.CargoController;
+import com.gutoconde.simws.api.controller.GabineteController;
 
 @SpringBootApplication
 @EnableScheduling
+@Import({CargoController.class, GabineteController.class})
 public class SimwsApplication {
 
 	public static void main(String[] args) {
